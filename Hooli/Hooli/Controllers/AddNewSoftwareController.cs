663ca5dc1fb3153fe6f1e4c35ac87e3851f1992e
ralowden <ralowden@indiana.edu>
2014-11-18 10:59:34 -0500

    First iteration of uploading software

--- Hooli/Hooli/Controllers/AddNewSoftwareController.cs
+++ Hooli/Hooli/Controllers/AddNewSoftwareController.cs
@@ -16,5 +16,23 @@ namespace Hooli.Controllers
             return View();
         }
 
+        public ActionResult Save(FormCollection formCollection)
+        {
+            if(Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+
+                if((file!=null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    //code for saving data to filesystem
+                }
+            }
+            return View("Index");
+        }
+
     }
 }
