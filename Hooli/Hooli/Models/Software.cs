0287e0253d287087e54fff445383e7dacfe2bf44
ralowden <ralowden@indiana.edu>
2014-11-18 13:04:15 -0500

    Add comments to uploaded software

---
+++ Hooli/Hooli/Models/Software.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hooli.Models
+{
+    public class Software
+    {
+        public int id { get; set; }
+        public int admin_id { get; set; }
+        public string name { get; set; }
+        public string version { get; set; }
+        public DateTime date_added { get; set; }
+        [Display(Name = "Description")]
+        public string description { get; set; }
+        public string download_link { get; set; }
+        //make static?
+        public int downloads { get; set; }
+    }
+}
