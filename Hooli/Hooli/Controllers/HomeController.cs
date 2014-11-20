107eb4271f9e50c94acc78edb4385539cc5f1749
iweating <iweating@indiana.edu>
2014-11-19 20:26:30 -0500

    Isaiah Bogus

--- Hooli/Hooli/Controllers/HomeController.cs
+++ Hooli/Hooli/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Hooli.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Isaiah";
 
             return View();
         }
