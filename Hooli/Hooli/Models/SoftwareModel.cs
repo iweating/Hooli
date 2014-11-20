074893c17b4977542ea321a3a5713c8f5468fa7a
ralowden <ralowden@indiana.edu>
2014-11-19 20:26:07 -0500

    Rachel bogus

--- Hooli/Hooli/Models/SoftwareModel.cs
+++ Hooli/Hooli/Models/SoftwareModel.cs
@@ -11,13 +11,13 @@ namespace Hooli.Models
         public int id { get; set; }
         public int admin_id { get; set; }
         [Display(Name="Software name")]
-        public string name { get; set; }
-        [Display(Name="Version number")]
+        public string softwareName { get; set; }
+        [Display(Name = "Version number")]
+        public string fileName { get; set; }
         public string version { get; set; }
         public DateTime date_added { get; set; }
         [Display(Name = "Description")]
         public string description { get; set; }
-        public string download_link { get; set; }
         public string data { get; set; }
         //public int downloads { get; set; }
     }
