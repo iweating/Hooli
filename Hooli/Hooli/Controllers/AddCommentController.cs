6e6128a5a94ce94b9de0f9cb169021fb1d8b9f01
smgehrig <smgehrig@indiana.edu>
2014-11-30 14:14:41 -0500

    comments

---
+++ Hooli/Hooli/Controllers/AddCommentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Hooli.Models;
+using Hooli.MySql;
+
+namespace Hooli.Controllers
+{
+    public class AddCommentController : Controller
+    {
+        //
+        // GET: /AddComment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+        public ActionResult Submit(FormCollection formCollection)
+        {
+            String text = formCollection.Get("comment_text"), query;
+            MySqlCommand cmd = new MySqlCommand();
+            query = "insert into comments (comment, Software_ID) values (@text, )";
+            cmd.CommandText = query;
+            cmd.Parameters.Add("@text", MySqlDbType.String).Value = text;
+        return View("Index");
+        }
+    }
+    
+}
