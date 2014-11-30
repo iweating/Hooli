fe75f2abf5d8e6d312ff50f6d3bfcfe5f8616ac8
smgehrig <smgehrig@indiana.edu>
2014-11-30 14:58:10 -0500

    comments

--- Hooli/Hooli/Controllers/AddCommentController.cs
+++ Hooli/Hooli/Controllers/AddCommentController.cs
@@ -24,9 +24,12 @@ namespace Hooli.Controllers
         {
             String text = formCollection.Get("comment_text"), query;
             MySqlCommand cmd = new MySqlCommand();
-            query = "insert into comments (comment, Software_ID) values (@text, )";
+            query = "insert into comments (comment, Software_ID, Date) values (@text, 7, @date)";
             cmd.CommandText = query;
             cmd.Parameters.Add("@text", MySqlDbType.String).Value = text;
+            cmd.Parameters.AddWithValue("@date", DateTime.Now);
+            DBConnect db = new DBConnect();
+            db.Insert(cmd);
         return View("Index");
         }
     }
