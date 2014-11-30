a801043fbeb222a66ba3ac8d0ad9fdcb7dc7af2c
lingor93 <lingor93@gmail.com>
2014-11-30 17:37:23 -0500

    Profile view

--- Hooli/Hooli/Controllers/ProfileController.cs
+++ Hooli/Hooli/Controllers/ProfileController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Hooli.MySql;
+using MySql.Data.MySqlClient;
+using Hooli.Models;
+using System.Data;
 
 namespace Hooli.Controllers
 {
@@ -14,9 +17,134 @@ namespace Hooli.Controllers
 
         public ActionResult Index()
         {
-            DBConnect connect = new DBConnect();
-            //connect.Insert("Insert into Ratings values(1,1,1,3,3,3,3,8,11/16/14)");
-            return View();
+            DBConnect db = new DBConnect();
+            string download_query = "SELECT * FROM Download_history";
+            string profile_query = "SELECT * FROM User";
+
+            MySqlCommand download_cmd = new MySqlCommand(download_query);
+            MySqlCommand profile_cmd = new MySqlCommand(profile_query);
+
+            var profile_model = FillProfileModel(profile_cmd);
+            var download_model = FillDownloadHistoryModel(download_cmd);
+
+            ProfileDownloadViewModel model = new ProfileDownloadViewModel();
+            model.profile = profile_model;
+
+            return View(profile_model);
+        }
+
+        public FileResult Download()
+        {
+            DBConnect db = new DBConnect();
+            string user_id = (string)RouteData.Values["user_id"];
+            string query = "select * from User where user_id = " + user_id + ";";
+            MySqlCommand cmd = new MySqlCommand(query);
+            DataTable dt = db.GetData(cmd);
+            Byte[] bytes = (Byte[])dt.Rows[0]["data"];
+            string contentType = (string)dt.Rows[0]["contentType"];
+            string fileName = (string)dt.Rows[0]["fileName"];
+            UpdateDownloadCount(user_id, (int)dt.Rows[0]["downloads"]);
+            return File(bytes, contentType, fileName);
+        }
+
+        public ActionResult SearchHistory(MySqlCommand cmd)
+        {
+            DBConnect db = new DBConnect();
+            string user_id = (string)RouteData.Values["user_id"];
+            string query = "select user_id from User where user_id = @user_id";
+            cmd.CommandText = query;
+            cmd.Parameters.Add("@softwareName", MySqlDbType.String).Value = softwareName;
+            var model = FillSoftwareModel(cmd);
+            return View("Index", model);
+
+            }
+            else if (checkedButton == "contains")
+            {
+                List<SoftwareModel> software = new List<SoftwareModel>();
+                query = "select * from Software";
+                cmd.CommandText = query;
+                var allSoftware = FillSoftwareModel(cmd);
+                if (allSoftware.Count() != 0)
+                {
+                    foreach (SoftwareModel model in allSoftware)
+                    {
+                        if (model.softwareName.Contains(softwareName))
+                        {
+                            software.Add(model);
+                        }
+                    }
+                    return View("Index", software);
+                }
+                else return View("Error");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        private IEnumerable<ProfileModel> FillProfileModel(MySqlCommand cmd)
+        {
+            DBConnect db = new DBConnect();
+            ProfileModel profile = new ProfileModel();
+            if (db.GetData(cmd) != null)
+            {
+                foreach (DataRow row in db.GetData(cmd).Rows)
+                {
+                    profile.Add(new ProfileModel()
+                    {
+                        user_id = (int)row["user_id"],
+                        FirstName = (string)row["FirstName"],
+                        LastName = (string)row["LastName"]
+                    });
+                }
+            }
+            IEnumerable<ProfileModel> model = profile;
+            return model;
+        }
+
+        private IEnumerable<DownloadHistoryModel> FillDownloadHistoryModel(MySqlCommand cmd)
+        {
+            DBConnect db = new DBConnect();
+            List<DownloadHistoryModel> download = new List<DownloadHistoryModel>();
+            if (db.GetData(cmd) != null)
+            {
+                foreach (DataRow row in db.GetData(cmd).Rows)
+                {
+                    download.Add(new DownloadHistoryModel()
+                    {
+                        user_id = (int)row["user_id"],
+                        softwareName = (string)row["softwareName"],
+                        version = (string)row["version"],
+                        id = (int)row["id"],
+                        download_data = (DateTime)row["download_data"],
+                        dl_version = (string)row["dl_version"]
+                    });
+                }
+            }
+            IEnumerable<DownloadHistoryModel> model = download;
+            return model;
+        }
+
+        private ProfileModel FillProfileModel(MySqlCommand cmd)
+        {
+            DBConnect db = new DBConnect();
+            ProfileModel profile = new ProfileModel();
+            if (db.GetData(cmd) != null)
+            {
+                                
+foreach (DataRow row in db.GetData(cmd).Rows)
+                {
+                    profile.Add(new ProfileModel()
+                    {
+                        user_id = (int)row["user_id"],
+                        FirstName = (string)row["FirstName"],
+                        LastName = (string)row["LastName"]
+                    });
+                }
+            }
+            ProfileModel model = profile;
+            return model;
         }
 
     }
---
+++ Hooli/Hooli/Models/ProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hooli.Models
+{
+    public class ProfileModel
+    {
+        public int user_id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
