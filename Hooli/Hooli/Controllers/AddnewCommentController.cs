7a57ef45e585838b21b092bc652e5938840b16c9
kingod180 <odonnell_johnny@yahoo.com>
2014-11-30 20:41:37 -0500

    Software rating updater

--- Hooli/Hooli/Controllers/AddnewCommentController.cs
+++ Hooli/Hooli/Controllers/AddnewCommentController.cs
@@ -23,7 +23,8 @@ namespace Hooli.Controllers
         [Authorize]
         public ActionResult Submit(FormCollection formCollection)
         {
-            int software_id_dummy_value = 7;
+            int software_id_dummy_value = 17;
+            int rating_dummy_value = 3;
 
             String text = formCollection.Get("comment_text"), query;
             MySqlCommand cmd = new MySqlCommand();
@@ -36,9 +37,12 @@ namespace Hooli.Controllers
 
             // Average new rating with current software rating
             String current_software_rating_query = "SELECT rating FROM Software where id=" + software_id_dummy_value;
-            MySqlCommand current_software_cmd = new MySqlCommand(current_software_rating_query);
-            db.GetData(current_software_cmd);
-
+            MySqlCommand current_software_rating_cmd = new MySqlCommand(current_software_rating_query);
+            int current_rating = (int) db.GetData(current_software_rating_cmd).Rows[0][0];
+            int new_rating = (int) (.75 * current_rating + .25 * rating_dummy_value);
+            String new_software_rating_query = "INSERT INTO Software (rating) VALUES ('" + new_rating+ "');";
+            MySqlCommand new_software_rating_cmd = new MySqlCommand(new_software_rating_query);
+            db.Insert(new_software_rating_cmd);
 
             return View("Index");
         }
